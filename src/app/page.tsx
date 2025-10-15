7f886dc9672425b2a96abe8d9d1deb2916069b89
BrunoItikawa10X <brunoitikawa@10xdigital.com.br>
2025-10-15 17:47:55 -0300

    inngest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 'use client'
 import LogoutButton from "./logout";
 import { useTRPC } from "@/trpc/client";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 
 
 const page = () => {  
   const trpc = useTRPC();
   const data = useQuery(trpc.getWorkflows.queryOptions());
-  const create = useMutation(trpc.createWorkflow.mutationOptions())
+  const queryClient = useQueryClient();
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () =>{
+      queryClient.invalidateQueries(trpc.getWorkflows.queryOptions())
+    }
+  }))
 
   return(
     <div className="h-screen min-w-sreen flex items-center justify-center flex-col gap-y-6">
